3057684ed6f9c9221811f43d6e0e6284c6563133
Trent Houliston <trent@houliston.me>
2015-09-24 06:14:00 +1000

    Can now send data from javascript and it is received properly

--- tests/test.js
+++ tests/test.js
@@ -10,8 +10,10 @@ nu.on('nuclear_leave', function (name) {
     console.log('Leave', name);
 });
 
-nu.on('std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >', function(source, data) {
+var stringName = 'std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >';
 
+nu.on(stringName, function(source, data) {
+    console.log('Message from', source);
     var string = data.toString();
 
     if(string.length < 100) {
@@ -22,3 +24,16 @@ nu.on('std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::alloc
     }
 
 });
+
+setTimeout(function() {
+    console.log('All Unreliable');
+    nu.send(stringName, new Buffer('Hello World from Javascript! (ALL UNRELIABLE)'));
+    console.log('All Reliable');
+    nu.send(stringName, new Buffer('Hello World from Javascript! (ALL RELIABLE)'), undefined, true);
+    console.log('nettest1 Unreliable');
+    nu.send(stringName, new Buffer('Hello World from Javascript! (nettest1 UNRELIABLE)'), 'nettest1');
+    console.log('nettest1 Reliable');
+    nu.send(stringName, new Buffer('Hello World from Javascript! (nettest1 RELIABLE)'), 'nettest1', true);
+    console.log('nettest1 Big');
+    nu.send(stringName, new Buffer(65535), 'nettest1');
+}, 2000)
